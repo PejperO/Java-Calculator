d12ef5cc3e29031c21a6bf29922664da7b205361
Sumii <64231313+PejperO@users.noreply.github.com>
2022-10-23 23:15:29 +0200

    Adding all files back

---
+++ src/Java/Calculator/CalculatorController.java
@@ -0,0 +1,92 @@
+package Java.Calculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class CalculatorController {
+
+    private long number1;
+    private String operator = "";
+    private boolean start = true;
+
+    @FXML
+    private Label output;
+
+    @FXML
+    private void processNumPad(ActionEvent event){
+        if(start) {
+            output.setText("");
+            start = false;
+        }
+
+        String value = ((Button)event.getSource()).getText();
+        output.setText(output.getText() + value);
+    }
+
+    @FXML
+    private void processOperator(ActionEvent event){
+        if(output.getText().equals("ERR"))
+            return;
+
+        String value = ((Button)event.getSource()).getText();
+
+        if(!value.equals("=")){
+            if(!operator.isEmpty()){
+                return;
+            }
+
+            operator = value;
+            number1 = Long.parseLong(output.getText());
+            output.setText("");
+        }
+
+        else{
+            if(operator.isEmpty()){
+                return;
+            }
+
+            if(output.getText().isEmpty()){
+                output.setText("ERR");
+                operator = "";
+                start = true;
+                return;
+            }
+
+            output.setText(operation(number1, Long.parseLong(output.getText()), operator));
+            operator = "";
+            start = true;
+        }
+    }
+
+    @FXML
+    private void clearOut(){
+        output.setText("0");
+        start = true;
+        operator = "";
+    }
+
+    @FXML
+    private void notWorking(){
+        System.out.println("this button does not work yet ^^ ");
+    }
+
+    String operation(long number1, long number2, String op){
+        switch (op){
+            case"+":
+                return String.valueOf(number1 + number2);
+            case"-":
+                return String.valueOf(number1 - number2);
+            case"×":
+                return String.valueOf(number1 * number2);
+            case"÷":
+                if(number2 == 0){
+                    return "ERR";
+                }
+                return String.valueOf(number1 / number2);
+            default:
+                return "ERR";
+        }
+    }
+}
---
+++ src/Java/Calculator/Main.java
@@ -0,0 +1,28 @@
+package Java.Calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("modern_calculator.fxml")));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setTitle("Java Calculator");
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
